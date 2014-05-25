aad5f81cc79e17842bf7a93ded5bd1853e0e5c33
southafricaew <aashith_kamath@hotmail.com>
2014-05-25 17:44:38 +0530

    Final Answer
    Program Assignment for Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,38 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+
 
 ## Write a short comment describing this function
-
+## This function will first take a square matrix argument as input.
+## Then it will define the set, get, setinverse, getinverse functions
+## for this matrix as the user can then access this object in another
+## function and uses the object's function. The output of this function
+## is a list that contains these functions.
 makeCacheMatrix <- function(x = matrix()) {
- 
-    m <- NULL
-    set <- function(y) {
-      x <<- y
-      m <<- NULL
-    }
-    get <- function() x
-    setmean <- function(mean) m <<- mean
-    getmean <- function() m
-    list(set = set, get = get,
-         setmean = setmean,
-         getmean = getmean)
-  
-
+  inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(Inverse) inverse <<- Inverse
+  getinverse <- function() inverse
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
-
+## This function will take the output from above function,
+## which is the special list that contains the matrix object's
+## functions. The output of this function will be the inverted 
+## matrix.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  
-  m <- x$getmean()
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
   if(!is.null(m)) {
-    message("getting cached data")
+    message("getting cached inverted matrix")
     return(m)
   }
-  data <- x$get()
-  m <- mean(data, ...)
-  x$setmean(m)
+  matrix <- x$get()
+  m <- solve(matrix, ...)
+  x$setinverse(m)
   m
 }
